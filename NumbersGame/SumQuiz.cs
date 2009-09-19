1058e849b86766603239d10617e1cdbe8fb42a5e
markheath <markheath@localhost>
2009-09-19 10:03:21 +0000

    Simplified NumbersViewModel by introducing SumQuiz Added Moq dependency

--- NumbersGame/SumQuiz.cs
+++ NumbersGame/SumQuiz.cs
@@ -7,6 +7,34 @@ namespace LearningGames.Numbers
 {
     public class SumQuiz
     {
-        public int RightAnswers { get; private set; }
+        ISumProvider sumProvider;
+        Sum currentSum;
+
+        public SumQuiz(ISumProvider sumProvider)
+        {
+            this.sumProvider = sumProvider;
+            this.currentSum = sumProvider.GetNextSum();
+        }
+
+        public int Score { get; private set; }
+
+        public bool SubmitAnswer(string answer)
+        {
+            bool correct = currentSum.IsCorrect(answer);
+            if (correct)
+            {
+                Score++;
+                currentSum = sumProvider.GetNextSum();
+            }
+            return correct;
+        }
+
+        public Sum CurrentSum
+        {
+            get
+            {
+                return currentSum;
+            }
+        }
     }
 }
