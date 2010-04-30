582c82c8dee50b5d217e37976a938d8749cac441
markheath <markheath@localhost>
2010-04-30 11:07:00 +0000

    difficulty level selection for numbers game

---
+++ LearningGames.Framework/NavigateMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningGames.Framework
+{
+    public class NavigateMessage
+    {
+        public object Target { get; set; }
+    }
+}
