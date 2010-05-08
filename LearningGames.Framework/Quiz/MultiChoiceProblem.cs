8affccdcee158dac96efcb98202f7d44d8f78cb2
Mark Heath <147668+markheath@users.noreply.github.com>
2010-05-08 22:50:32 +0000

    added a multiple choice question type (find the biggest)

---
+++ LearningGames.Framework/Quiz/MultiChoiceProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace LearningGames.Framework.Quiz
+{
+    public abstract class MultiChoiceProblem : Problem
+    {
+        private static MultiChoiceProblemPresenter presenter = new MultiChoiceProblemPresenter();
+        public abstract ICollection<object> Choices { get; }
+        public abstract int CorrectItemIndex { get; }
+
+        public override FrameworkElement Presenter
+        {
+            get { return presenter; }
+        }
+
+        public override object GetViewModel()
+        {
+            return new MultiChoiceViewModel(this);
+        }
+    }
+}
